a52b7dff7a9c46659d5e8326ec573be4cd96b44d
Ben O'Hara <bpo@robotparade.co.uk>
2011-04-05 22:00:46 +0100

    Added stylesheet to style buttons. Changed onclick to retrieve appropriate data from the track clicked.

--- musicbrainz.fm.js
+++ musicbrainz.fm.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
 	var imageUrl=chrome.extension.getURL("audioscrobbler.png");
-   $('.track .title').append($('<img />').attr('src',imageUrl).attr('title','scrobble').click(function(){
-   	alert('wow')
-   }));
+   $('.track .title').prepend(
+   		$('<img />')
+			.attr('src',imageUrl)
+			.attr('title','Scrobble')
+			.addClass('scrobblebutton')
+			.click(function(){
+				var trackEl=$(this).next();
+		   		var track,artist,mbid;
+				track=trackEl.text();
+				artist=$('.artisttitle tr td.title a[title="Go to the artist page"]').text();
+				mbid=trackEl.attr("href").substring(7,43);
+				scrobble(track,artist,mbid);
+		   })
+   );
  });
+ 
+ 
+ function scrobble(track, artist, mbid)
+ {
+ 	alert('Scrobbling: \n' + 
+			'artist: ' + artist + '\n' +
+			'track: ' + track + '\n' +
+			'mbid: ' + mbid );
+ }
