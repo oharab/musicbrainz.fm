6f3c9449d0c5443b424c974577351ce2b76e817f
Ben O'Hara <bpo@robotparade.co.uk>
2011-06-18 00:18:08 +0100

    changed the adding of the scrobble buttons to work with the new musicbrainz.org layout.

--- musicbrainz.fm.js
+++ musicbrainz.fm.js
@@ -1,26 +1,40 @@
+jQuery.fn.log = function (msg) {
+      console.log("%s: %o", msg, this);
+      return this;
+  };
+
 $(document).ready(function() {
-	var imageUrl=chrome.extension.getURL("audioscrobbler.png");
-   $('.track .title').prepend(
+	
+    var imageUrl=chrome.extension.getURL("audioscrobbler.png");
+	console.log('Sorting out images...');
+	var artist=$('a[rel="foaf:maker"]').text();
+	console.log('artist=' + artist);
+	var album=$('a[rel="foaf:isPrimaryTopicOf"]').text();
+	console.log('album=' + album);
+   $('td > span[property="dct:title rdfs:label"]').prepend(
    		$('<img />')
 			.attr('src',imageUrl)
 			.attr('title','Scrobble')
 			.addClass('scrobblebutton')
 			.click(function(){
-				var trackEl=$(this).next();
-		   		var track,artist,mbid;
+				var trackEl=$(this).parent().next();
+		   		var track,mbid;
 				track=trackEl.text();
-				artist=$('.artisttitle tr td.title a[title="Go to the artist page"]').text();
-				mbid=trackEl.attr("href").substring(7,43);
-				scrobble(track,artist,mbid);
+				mbid=trackEl.attr("href").substring(33,99);
+				scrobble(artist,album,track,mbid);
 		   })
    );
  });
  
  
- function scrobble(track, artist, mbid)
+ 
+ function scrobble(artist,album,track,mbid)
  {
+ 	console.log('scrobbling %s %s %s %s',artist,album,track,mbid);
+	console.log('md5: %s',hex_md5(artist));
 	chrome.extension.sendRequest({'action' : 'scrobbleTrack',
 								 	'artist':artist,
+									'album':album,
 									'track':track,
 									'mbid':mbid
 	},function(result){
