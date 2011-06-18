d1c6bfebcf5bc5ff58c3641376ebfd692c4001d0
Ben O'Hara <bpo@robotparade.co.uk>
2011-06-18 01:36:53 +0100

    fixed code so content script calls background page. Added basic page action to display scrobbler logo on musicbrainz websites.

--- musicbrainz.fm.js
+++ musicbrainz.fm.js
@@ -1,12 +1,4 @@
-jQuery.fn.log = function (msg) {
-      console.log("%s: %o", msg, this);
-      return this;
-  };
-
-$(document).ready(function() {
-	
     var imageUrl=chrome.extension.getURL("audioscrobbler.png");
-	console.log('Sorting out images...');
 	var artist=$('a[rel="foaf:maker"]').text();
 	console.log('artist=' + artist);
 	var album=$('a[rel="foaf:isPrimaryTopicOf"]').text();
@@ -24,24 +16,18 @@ $(document).ready(function() {
 				scrobble(artist,album,track,mbid);
 		   })
    );
- });
  
  
  
  function scrobble(artist,album,track,mbid)
  {
  	console.log('scrobbling %s %s %s %s',artist,album,track,mbid);
-	console.log('md5: %s',hex_md5(artist));
-	chrome.extension.sendRequest({'action' : 'scrobbleTrack',
-								 	'artist':artist,
-									'album':album,
-									'track':track,
-									'mbid':mbid
-	},function(result){
-		
-		if(!result.success)
-			return;
-	});
+	chrome.extension.sendRequest({'artist' : artist
+								 ,'album' : album
+								 ,'track' : track
+								 ,'mbid' : mbid}, onScrobbled);
  }
  
- 
+ function onScrobbled(result){
+ 	console.log('scrobbled: %s',result);
+ }
